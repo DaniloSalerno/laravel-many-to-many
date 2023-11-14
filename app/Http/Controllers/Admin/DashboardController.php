0501fd46b37b4e460a6743bfd2199edb0b72f1b6
DaniloSalerno <salerno.danilo@hotmail.it>
2023-11-14 14:13:06 +0100

    initial commit

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Type;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $total_projects = Project::all()->count();
+        $total_users = User::all()->count();
+        $total_types = Type::all()->count();
+        return view('admin.dashboard', compact('total_projects', 'total_users', 'total_types'));
+    }
+}
